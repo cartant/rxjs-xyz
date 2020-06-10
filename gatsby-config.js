7cbe1178d5fd93c7812f85474f5ea253007a4fc7
Nikolai Prokoschenko <nikolai.prokoschenko@kurzdigital.com>
2020-06-10 10:30:25 +0200

    feat: add rss feed for new entries

--- gatsby-config.js
+++ gatsby-config.js
@@ -87,6 +87,7 @@ module.exports = {
     },
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-offline`,
+    `gatsby-plugin-feed`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-typography`,
